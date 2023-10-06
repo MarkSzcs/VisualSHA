ad69a9fe2c19fc584fb3340a7ddb6c3cabaec367
CASOYC <casoyc@inf.elte.hu>
2023-10-06 16:31:06 +0200

    Binary handling done

---
+++ binaryHandling/Binary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binaryHandling
+{
+    public class Binary
+    {
+        public List<byte> BinaryValue { get; set; }
+
+        public Binary(string input)
+        {
+            byte[] binArr = Encoding.UTF8.GetBytes(input);
+            BinaryValue = new List<byte>(binArr);
+        }
+
+        public Binary(IEnumerable<byte> input)
+        {
+            BinaryValue = new List<byte>(input);
+        }
+
+        public void RightShift(int shiftVal)
+        {
+            BinaryValue = BinaryValue.Select(b => (byte)(b >> shiftVal)).ToList();
+        }
+
+        public void LeftShift(int shiftVal)
+        {
+            BinaryValue = BinaryValue.Select(b => (byte)(b << shiftVal)).ToList();
+        }
+
+        public override string ToString()
+        {
+            return new StringBuilder().AppendJoin("", BinaryValue).ToString();
+        }
+
+        public string ToHex()
+        {
+            return new BigInteger(BinaryValue.ToArray()).ToString("X");
+        }
+    }
+}
---
+++ binaryHandling/BinaryHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binaryHandling
+{
+    public static class BinaryHandler
+    {
+        public static Binary And(Binary num1, Binary num2)
+        {
+            Binary result = new(num1.BinaryValue.Zip(num2.BinaryValue, (b1, b2) => (byte)(b1 & b2)).ToList());
+            return result;
+        }
+
+        public static Binary Or(Binary num1, Binary num2)
+        {
+            Binary result = new(num1.BinaryValue.Zip(num2.BinaryValue, (b1, b2) => (byte)(b1 | b2)).ToList());
+            return result;
+        }
+
+        public static Binary Xor(Binary num1, Binary num2)
+        {
+            Binary result = new(num1.BinaryValue.Zip(num2.BinaryValue, (b1, b2) => (byte)(b1 ^ b2)).ToList());
+            return result;
+        }
+    }
+}
